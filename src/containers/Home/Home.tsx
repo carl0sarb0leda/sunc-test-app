9c87d364ba6cfef76dae376d5a5b7462a894518e
Carlos <carlos@tradewindow.io>
2023-03-01 00:18:31 +1300

    Add logic to fetch albums api in home container

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { AlbumsTable } from "components";
+import React, { useEffect, useState } from "react";
+import { AlbumApiProps } from "types/api";
+import { getAlbums } from "utils/api-service";
 
 export const Home = () => {
-    return <div>Home</div>;
+    const [albumsData, setAlbumsData] = useState<AlbumApiProps[] | null>(null);
+    const [isLoadingAlbums, setIsLoadingAlbums] = useState(false);
+
+    useEffect(() => {
+        const getAlbumsData = async () => {
+            setIsLoadingAlbums(true);
+            const response = await getAlbums();
+            setAlbumsData(response);
+            setIsLoadingAlbums(false);
+        };
+        getAlbumsData();
+    }, []);
+
+    if (isLoadingAlbums) {
+        return <div>Loading...</div>;
+    }
+    if (albumsData) {
+        return <AlbumsTable albumsData={albumsData} />;
+    }
+    return null;
 };
